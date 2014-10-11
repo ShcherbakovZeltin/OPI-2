e222687a3ab72dd332e003875e7dc77884003f47
Timofey Shcherbakov <tim_ekb@mail.ru>
2014-10-11 21:00:15 +0400

    r1

---
+++ commits/commit1/E.java
@@ -0,0 +1,18 @@
+public class E {
+
+    private long e = 1234;
+
+    private long j = 1234;
+
+    public byte oo() {
+        return 2;
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+}
---
+++ commits/commit1/H.java
@@ -0,0 +1,18 @@
+public class H {
+
+    private double j = 100.500;
+
+    private long i = 4321;
+
+    public Object gg() {
+        return return getClass().getClassLoader();
+    }
+
+    public long dd() {
+        return 33;
+    }
+
+    public long ac() {
+        return 111;
+    }
+}
